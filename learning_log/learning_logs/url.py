0c2023d4867597480e423499ce57cf6b00f69016
liuhuan <huan.liu@foriseholdings.com>
2018-11-23 21:12:14 +0800

    增加表单提交功能

--- learning_log/learning_logs/url.py
+++ learning_log/learning_logs/url.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
   url(r'^$', views.index, name='index'),
   url(r'^topics/$', views.topics, name='topics'),
-  url(r'^topics/(?P<topic_id>\d+)/$',views.topic, name='topic')
+  url(r'^topics/(?P<topic_id>\d+)/$',views.topic, name='topic'),
+  url(r'^new_topic/$', views.new_topic, name='new_topic')
 ]
