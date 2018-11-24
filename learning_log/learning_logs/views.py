1dcc03bc2b29ff0085ea9bec2e63547403bd2d84
liuhuan <huan.liu@foriseholdings.com>
2018-11-25 00:00:56 +0800

    提交entry

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from .models import Topic
 
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 # Create your views here.
 def index(request):
   return render(request, 'learning_logs/index.html')
@@ -34,3 +34,21 @@ def new_topic(request):
 
   context = {'form': form}
   return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+  topic = Topic.objects.get(id=topic_id)
+  if request.method != 'POST':
+    # 如果没有提交数据,创建表单
+    form = EntryForm()
+  else:
+    form = EntryForm(data=request.POST)
+    if form.is_valid():
+      new_entry = form.save(commit=False)
+      new_entry.topic = topic
+      new_entry.save()
+      return HttpResponseRedirect(reverse('learning_logs:topic',
+                                          args=[topic_id]))
+
+  context = {'topic': topic,'form': form}
+  return render(request, 'learning_logs/new_entry.html', context)
